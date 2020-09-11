4a9e154ebf723ec59fbdd23139c22197def01846
Michael Oldacre <mloldacre@live.com>
2020-09-11 15:19:29 -0400

    Prep for deploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://lifescribe@localhost/lifescribe',
+  DATABASE_URL: process.env.DB_URL || 'postgresql://lifescribe@localhost/lifescribe',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://lifescribe@localhost/lifescribe-test',
   API_TOKEN: process.env.API_TOKEN,
 };
