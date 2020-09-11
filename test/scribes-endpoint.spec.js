43ef581baced0e5339e282b6d7a687ee81407010
Michael Oldacre <mloldacre@live.com>
2020-09-11 15:22:03 -0400

    Fixed names

--- test/scribes-endpoint.spec.js
+++ test/scribes-endpoint.spec.js
@@ -13,7 +13,7 @@ describe.only('Scribes Endpoints', () => {
   before('make knex instance', () => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL,
+      connection: process.env.TEST_DATABASE_URL,
     });
     app.set('db', db);
   });
